eb19e611060341bc471e03f7e6f96111281d3011
nengsyahla <115355205+nengsyahla@users.noreply.github.com>
2022-10-08 20:07:15 +0700

    Add files via upload

---
+++ HandPhone.java
@@ -0,0 +1,20 @@
+public class HandPhone {
+    String jenis_hp;
+    int tahun_pembuatan;
+    public void setDataHP(String jenis_hp, int tahun_pembuatan){
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+    String getJenisHP(){
+        return jenis_hp;
+    }
+    long getTahunPembuatan(){
+        return tahun_pembuatan;
+    }
+    public static void main(String[] args){
+        HandPhone hp = new HandPhone();
+        hp.setDataHP("Oppo", 2020);
+        System.out.println(hp.getJenisHP());
+        System.out.println(hp.getTahunPembuatan());
+    }
+}
